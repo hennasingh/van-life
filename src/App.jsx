f89a806dc84bebf4b715d93de3c96d1344d139eb
Henna Singh <hennasingh04@gmail.com>
2024-01-15 08:39:02 +0000

    separated header to layout component and used Outlet concept to render header in all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from './pages/Home'
 import About from './pages/About'
-import Vans from './pages/Vans'
-import VanDetail from "./pages/VanDetail"
+import Vans from './pages/Vans/Vans'
+import VanDetail from "./pages/Vans/VanDetail"
+import Layout from "./components/Layout"
 import './App.css'
 
 import "./server"
@@ -11,18 +12,13 @@ function App() {
 
   return (
     <BrowserRouter>
-        <header className="site-header">
-          <Link className="site-logo" to="/">#VANLIFE</Link>           
-          <nav>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
-         </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path = "/vans/:id" element= {<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path = "/vans/:id" element= {<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
