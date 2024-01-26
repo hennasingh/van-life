4beacb2bc49f6dbd2f989218354ce35d420dc719
Henna Singh <hennasingh04@gmail.com>
2024-01-26 13:39:30 +0000

    added nested route in hostVans and additional pages for details, pricing and photos. Set up useSearchParams to filter van type

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,12 @@ import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 import HostVans from "./pages/Host/HostVans"
 import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import './App.css'
-
 import "./server"
+
 
 
 function App() {
@@ -30,7 +33,11 @@ function App() {
             <Route index element={<Dashboard />} />
             <Route path="income" element= {<Income />} />
             <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />} />
+            <Route path="vans/:id" element={<HostVanDetail />} >
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
             <Route path="reviews" element= {<Reviews />} />
           </Route>
         </Route>
--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, Outlet, NavLink } from 'react-router-dom'
 
 export default function HostVanDetail() {
 
@@ -14,6 +14,12 @@ export default function HostVanDetail() {
 
     if (!currentVan) {
         return <h1>Loading...</h1>
+    }
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration:"underline",
+        color: "#161616"
     }
 
     return (
@@ -36,6 +42,29 @@ export default function HostVanDetail() {
                         <h4>${currentVan.price}/day</h4>
                     </div>
                 </div>
+                <nav className="host-van-detail-nav">
+                <NavLink 
+                    to="."
+                    end
+                    style={({isActive}) => isActive? activeStyles : null }
+                >
+                    Details
+                </NavLink>
+                <NavLink 
+                    to="pricing"
+                    style={({isActive}) => isActive? activeStyles : null }
+                >
+                    Pricing
+                </NavLink>
+                <NavLink 
+                    to="photos"
+                    style={({isActive}) => isActive? activeStyles : null }
+                >
+                    Photos
+                </NavLink>
+
+                </nav>
+                <Outlet context={{ currentVan }}/>
             </div>
         </section>
     )
