fcfa6d0c7e7928255ff8eb3aeb0c89710b676f44
Henna Singh <hennasingh04@gmail.com>
2024-01-29 07:45:08 +0000

    implemented page for 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import HostVanPricing from "./pages/Host/HostVanPricing"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import './App.css'
 import "./server"
+import NotFound from "./pages/NotFound"
 
 
 
@@ -32,14 +33,15 @@ function App() {
           <Route path="host" element= {<HostLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="income" element= {<Income />} />
+            <Route path="reviews" element= {<Reviews />} />
             <Route path="vans" element={<HostVans />} />
             <Route path="vans/:id" element={<HostVanDetail />} >
                 <Route index element={<HostVanInfo />} />
                 <Route path="pricing" element={<HostVanPricing />} />
                 <Route path="photos" element={<HostVanPhotos />} />
             </Route>
-            <Route path="reviews" element= {<Reviews />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
