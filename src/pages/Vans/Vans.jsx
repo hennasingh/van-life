353c264b5f5dde884f69134f22231577936a9b44
Henna Singh <hennasingh04@gmail.com>
2024-01-29 08:30:00 +0000

    implemented api fetch code using async function in a separate api.js file and added a login page

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, useSearchParams } from 'react-router-dom'
+import { getVans } from "../../api"
 
 export default function Vans() {
 
@@ -8,11 +9,23 @@ export default function Vans() {
     const typeFilter = searchParams.get("type")
 
     const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
 
     React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getVans()
+                setVans(data)
+            } catch(err) {
+                console.log("There was an error!")
+                setError(err)
+            } finally {
+                setLoading(false)
+            }                  
+        }       
+        loadVans()
     }, [])
 
     const displayedVans = typeFilter
@@ -37,6 +50,14 @@ export default function Vans() {
             </div>
        
     ))
+
+    if(loading){
+        return <h1 aria-live="polite">Loading...</h1>
+    }
+
+    if (error) {
+        return <h1 aria-live="assertive">There was an error: {error.message}</h1>
+    }
 
     return (
         <div className="van-list-container">
